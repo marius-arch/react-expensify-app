2d446ae6cbbe4667c6334c61dba48cf3e18efc41
marius.starke <marius.starke@flyeralarm-industrialprint.com>
2021-11-29 11:08:31 +0100

    updated src folder

--- src/selectors/expenses-total.js
+++ src/selectors/expenses-total.js
@@ -1,2 +1,5 @@
-export default (expenses) =>
-    expenses.reduce((count, { amount }) => count + amount, 0);
+export default (expenses) => {
+  return expenses
+      .map((expense) => expense.amount)
+      .reduce((sum, value) => sum + value, 0);
+};
